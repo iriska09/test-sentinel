cafda3e648bd285b800dce7c862e520c643a4e1d
iriska09 <kudaibergenovayryska09@gmail.com>
2025-02-26 14:56:12 -0600

    changed the main file

--- main.tf
+++ main.tf
@@ -1,29 +1,58 @@
-# main.tf
+# provider "aws" {
+#   region = "us-west-2"
+# }
+
+# resource "aws_security_group" "example" {
+#   name        = "sentinel"
+#   description = "Example security group"
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "sentinel-sg"
+#   }
+# }
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-027951e78de46a00e"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "sentinel-instance"
+#   }
+# }
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "example" {
-  name        = "sentinel"
-  description = "Example security group"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sentinel-sg"
-  }
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-027951e78de46a00e"
+  ami           = "ami-027951e78de46a00e" # Replace with a valid AMI ID
   instance_type = "t2.micro"
 
   tags = {
-    Name = "sentinel-instance"
+    Name = "sentinel-demo-instance"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "sentinel-demo-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # This is what we want to block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
